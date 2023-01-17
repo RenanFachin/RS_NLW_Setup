327954efdaeff6c2b65382d0ef86f78e4d59c4f2
Renan <renan.fachin@hotmail.com>
2023-01-17 11:57:12 -0300

    initialize fastify and create first route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,16 @@
-console.log('funcionou')
+import Fastify from "fastify";
+
+// Executando o fastify
+const app = Fastify()
+
+
+// Rota
+app.get('/', () => {
+    return 'Hello World'
+})
+
+
+// Deixar o app "ouvindo" na porta 3333
+app.listen({
+    port: 3333
+})
