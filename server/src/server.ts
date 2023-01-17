580b44226afe0e380b5db8061fc11b6f08d763ce
Renan <renan.fachin@hotmail.com>
2023-01-17 13:44:30 -0300

    configure cross-origin-resource-sharing (CORS)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,25 @@
 import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
 
 // Executando o fastify
 const app = Fastify()
+// Executando o prismaClient
+const prisma = new PrismaClient()
 
+// Habilitando o Cross-Origin-Resource-Sharing
+app.register(cors)
 
 // Rota
-app.get('/', () => {
-    return 'Hello World'
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Beber'
+            }
+        }
+    })
+    return habits
 })
 
 
