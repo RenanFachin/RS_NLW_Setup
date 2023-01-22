e2de3b7174cc429ea8f283b98fad6f8b049b7776
Renan <renan.fachin@hotmail.com>
2023-01-22 13:16:45 -0300

    mobile: add animations to components

--- mobile/src/components/CheckBox.tsx
+++ mobile/src/components/CheckBox.tsx
@@ -2,6 +2,8 @@ import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-nativ
 import { Feather } from '@expo/vector-icons'
 import colors from "tailwindcss/colors";
 
+// Animações
+import Animated, { FlipInYRight, FadeOut } from "react-native-reanimated";
 
 interface Props extends TouchableOpacityProps {
     title: string;
@@ -9,27 +11,44 @@ interface Props extends TouchableOpacityProps {
 }
 
 export function CheckBox({ title, checked = false, ...rest }: Props) {
+
     return (
         <TouchableOpacity activeOpacity={0.7} className="flex-row mb-2 items-center" {...rest}>
             {
                 checked ?
-                    <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
-                        <Feather
-                            name="check"
-                            size={20}
-                            color={colors.white}
-                        />
-                    </View>
+                    <>
+                        <Animated.View
+                            className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+                            entering={FlipInYRight}
+                            exiting={FadeOut}
+                        >
+                            <Feather
+                                name="check"
+                                size={20}
+                                color={colors.white}
+                            />
+                        </Animated.View>
+
+                        <Animated.Text
+                            className="text-gray-400 line-through text-base ml-3 font-semibold"
+                            entering={FlipInYRight}
+                            exiting={FadeOut}
+                        >
+                            {title}
+                        </Animated.Text>
+                    </>
 
                     :
 
-                    <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
+                    <>
+                        <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
+
+                        <Text className="text-white text-base ml-3 font-semibold">
+                            {title}
+                        </Text>
+                    </>
             }
 
-
-            <Text className="text-white text-base ml-3 font-semibold">
-                {title}
-            </Text>
 
         </TouchableOpacity>
     )
--- mobile/src/components/ProgressBar.tsx
+++ mobile/src/components/ProgressBar.tsx
@@ -1,15 +1,32 @@
+import { useEffect } from 'react';
 import { View } from 'react-native'
+import Animated, { useAnimatedStyle, useSharedValue, withTiming, withDelay } from 'react-native-reanimated';
 
 interface ProgressBarProps {
     progress?: number;
 }
 
-export function ProgressBar({ progress = 70 }: ProgressBarProps) {
+export function ProgressBar({ progress = 0 }: ProgressBarProps) {
+
+    const sharedProgress = useSharedValue(progress)
+
+    const style = useAnimatedStyle(() => {
+        return {
+            // propriedades de estilização que queremos animar
+            width: `${sharedProgress.value}%`
+        }
+    })
+
+    useEffect(() => {
+        sharedProgress.value = withDelay(100, withTiming(progress))
+    }, [progress])
+
     return (
         <View className='w-full h-3 rounded-xl bg-zinc-700 mt-4'>
-            <View 
+            <Animated.View
                 className='h-3 rounded-xl bg-violet-600'
-                style={{width: `${progress}%`}}
+                // Aplicando o react-native-reanimated na transição de progresso
+                style={style}
             />
         </View>
     )
